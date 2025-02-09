13b0ae07d804ba20d9ea17c5e1e3ec87a16c18be
syed masood hussain <syedmasoodhussain551@gmail.com>
2025-02-09 16:04:54 -0500

    Reverted About component to include 'JAVA FULL STACK DEVELOPER

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ export default function About() {
             transition={{ duration: 0.8, delay: 0.2 }}
           >
             <p className="font-sans leading-relaxed">
-               Full Stack Developer with over 4+ years of hands-on experience, specializing in the end-to-end development
+               JAVA FULL STACK DEVELOPER with over 4+ years of hands-on experience, specializing in the end-to-end development
               of web applications and services in the Financial and IT industry. Proficient in various technologies,
               including J2EE, Spring Boot, and modern front-end frameworks such as React.js, Angular, HTML5, CSS3,
               and JavaScript.
